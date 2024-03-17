839035f9ca8813a15af370a999c0ffb176b6213e
moatazmujahid99 <forprofmoataz500@gmail.com>
2024-03-17 02:25:28 +0200

    add logout button

--- src/main/java/com/electro/presentation/controllers/LogoutController.java
+++ src/main/java/com/electro/presentation/controllers/LogoutController.java
@@ -19,6 +19,6 @@ public class LogoutController extends HttpServlet {
             session.invalidate();
         }
 
-        resp.sendRedirect(req.getRequestURI());
+        resp.sendRedirect("/home");
     }
 }
